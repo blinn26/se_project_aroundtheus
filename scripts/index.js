84341888bef2b5c0536dfd916867ba6ee928f4cb
Ben Linn <benlinn26@gmail.com>
2022-10-15 17:42:01 -0700

    intial cards setup

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+const intialCards = [
+    {
+        name: "Yosemite Valley",
+        link: "../images/yosemite.jpg",
+    },
+    {
+        name: "Lake Louise",
+        link: "../images/lake-louise.jpg",
+    },
+]
