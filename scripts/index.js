93fb9e1897a5ab9ce6d7a7fd91f2047f4bbe7d5c
Ben Linn <benlinn26@gmail.com>
2022-10-17 09:56:35 -0700

    cards from sprint 4 tutor help

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,26 @@
-const intialCards = [
-    {
-        name: "Yosemite Valley",
-        link: "../images/yosemite.jpg",
-    },
-    {
-        name: "Lake Louise",
-        link: "../images/lake-louise.jpg",
-    },
-]
+const initialCards = [
+  {
+    name: 'Yosemite Valley',
+    link: 'https://code.s3.yandex.net/web-code/yosemite.jpg',
+  },
+  {
+    name: 'Lake Louise',
+    link: 'https://code.s3.yandex.net/web-code/lake-louise.jpg',
+  },
+  {
+    name: 'Bald Mountains',
+    link: 'https://code.s3.yandex.net/web-code/bald-mountains.jpg',
+  },
+  {
+    name: 'Latemar',
+    link: 'https://code.s3.yandex.net/web-code/latemar.jpg',
+  },
+  {
+    name: 'Vanoise National Park',
+    link: 'https://code.s3.yandex.net/web-code/vanoise.jpg',
+  },
+  {
+    name: 'Lago di Braies',
+    link: 'https://code.s3.yandex.net/web-code/lago.jpg',
+  },
+];
