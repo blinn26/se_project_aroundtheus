0dc6bc4f893bf75ab4db28951632d423ff1369f4
Ben Linn <benlinn26@gmail.com>
2022-10-28 16:51:27 -0700

    braindead

--- scripts/index.js
+++ scripts/index.js
@@ -38,3 +38,33 @@ console.log(profileModalCloseButton)
 profileModalCloseButton.addEventListener("click", () => {
   modal.classList.remove("modal__opened")
 })
+
+/* const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+
+// find the form in the DOM
+const profileFormElement = // Use the querySelector() method
+
+// find the form fields in the DOM
+const nameInput = // Use querySelector()
+const jobInput = // Use querySelector()
+
+// find the form fields in the DOM
+const profileName = // Use querySelector()
+const profileJob = // Use querySelector()
+
+// the form submission handler. Note that its name 
+// starts with a verb and concisely describes what it does
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault(); 
+ 
+  // get the values of each field from the value property 
+  // of the corresponding input element
+
+  // insert new values into the textContent property of the 
+  // corresponding profile elements
+}
+
+// connect the handler to the form:
+// it will watch the submit event
+formElement.addEventListener('submit', handleProfileFormSubmit); */
