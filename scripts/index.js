f851d1ae246bd714bed5fdb7c3b8377b85a77eeb
Ben Linn <benlinn26@gmail.com>
2022-10-30 13:33:49 -0700

    index js tweaks

--- scripts/index.js
+++ scripts/index.js
@@ -28,7 +28,7 @@ const initialCards = [
 ];
 
 
- // const commands, functions, strings and eventListeners!
+// const commands, functions, strings and eventListeners!
 
 const cardTemplate = document.querySelector("#card-template").content.firstElementChild;
 const profileEditButton = document.querySelector(".profile__edit-button");
@@ -41,11 +41,24 @@ const profileLineInputValue = profileEditForm.querySelector(".modal__input-line"
 const profileInputValue = profileEditForm.querySelector(".modal__input-description");
 const cardListEl = document.querySelector(".cards__list");
 
+function createCard({ image, title }) { // data is an object 
+ // const templateNode = document.getElementById(here_is_a_variable_with_teplate_selector);
+  // take a node from templateNode
+  /*
+  * fill templateNode with values
+  * call renderCard({ what: template of the node with filled values, where: whre do we need to render it? this is our <UL>});
+  */
+}
 
+function renderCard({ what, where }) {
+  where.appendChild(what);
+}
 
-initialCards.forEach(function (cardData) {
-  
-  // clone template
+// loop -> createCard -> renderCard
+//user event handler -> createCard -> renderCard
+
+initialCards.forEach(cardData => {
+// clone template
   const cardEl = cardTemplate.cloneNode(true);
   // find .card__image
   const imageEl = cardEl.querySelector(".card__image");
@@ -59,6 +72,8 @@ initialCards.forEach(function (cardData) {
   cardTitle.textContent = cardData.name;
   // append to the list
   cardListEl.appendChild(cardEl);
+
+  createCard(cardData);
 });
 
 function openModal() {
@@ -79,6 +94,15 @@ function closeModal() {
   profileModal.classList.remove("modal__opened");
 
 }
-  profileEditForm.addEventListener("submit", formSubmitHandler);
-  profileEditButton.addEventListener("click", openModal);
-  profileModalCloseButton.addEventListener("click", closeModal);
+profileEditForm.addEventListener("submit", formSubmitHandler);
+profileEditButton.addEventListener("click", openModal);
+profileModalCloseButton.addEventListener("click", closeModal);
+
+
+/* const cardEl = createCard(data); 
+  {
+    return cardEl;
+  }
+function renderCard(data) {
+  cardListEl.appendChild(cardEl);
+} */
