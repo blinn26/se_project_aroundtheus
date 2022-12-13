f9f90988102469dc05263395090808e1778d030a
Ben Linn <benlinn26@gmail.com>
2022-12-13 12:31:34 -0800

    lunch

--- scripts/card.js
+++ scripts/card.js
@@ -17,27 +17,39 @@ class Card {
     this._element
       .querySelector(".card__like-button")
       .addEventListener("click", this._handleLikeIcon);
+    console.log(this._element);
 
     this._element
       .querySelector(".card__image")
       .addEventListener("click", this._previewModalImage);
+    console.log(this._element);
 
     this._element
       .querySelector(".card__trash-button")
-      .addEventListener("click", this._cardTrashButton);
+      .addEventListener("click", this._handleDeleteCard);
+    console.log(this._element);
   }
 
   /* -------------------------------------------------------------------------- */
   /*                           HANDLE AND GET TEMPLATE                          */
   /* -------------------------------------------------------------------------- */
 
-  _handleLikeIcon() {
-    this._element;
-    querySelector(this._handleLikeIcon);
-    classList.toggle(".card__like-button");
-    // like icon active
-  }
+  _previewModalImage = () => {
+    this._element.querySelector(".card__image").classList.toggle("card__image");
+    // image active
+  };
 
+  _handleLikeIcon = () => {
+    this._element
+      .querySelector(".card__like-button")
+      .classList.toggle("card__like-button_is-active");
+    // like icon active
+  };
+
+  _handleDeleteCard = () => {
+    this._element.remove();
+    // delete card
+  };
   _getTemplate() {
     const cardEl = document
       .querySelector(this._cardSelector)
