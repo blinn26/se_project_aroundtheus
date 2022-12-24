903f95511be619420b00253b32e68880d6d37fe5
Ben Linn <benlinn26@gmail.com>
2022-12-24 11:41:40 -0800

    section

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@
 /* -------------------------------------------------------------------------- */
 import '../pages/index.css'
 import '../scripts/constants.js'
+import Section from '../scripts/Section.js'
 import FormValidator from '../scripts/FormValidator.js'
 import Card from '../scripts/Card.js'
 import { openModal, closeModal } from '../scripts/utils.js'
