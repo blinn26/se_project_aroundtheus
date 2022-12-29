1788185906afa410a2359217ef2560aae28d49d3
Ben Linn <benlinn26@gmail.com>
2022-12-28 17:49:17 -0800

    update to rewatch

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,10 @@
 /* -------------------------------------------------------------------------- */
 /*                           IMPORT ALL THE CLASSES                           */
 /* -------------------------------------------------------------------------- */
-import '../pages/index.css'
-import '../scripts/constants.js'
-import { initialCards, selectors } from '../scripts/constants.js'
-import Section from '../scripts/Section.js'
-import FormValidator from '../scripts/FormValidator.js'
-import Card from '../scripts/Card.js'
-import { openModal, closeModal } from '../scripts/utils.js'
 
 /* -------------------------------------------------------------------------- */
 /*                        CREATE INSTANCE OF THE CLASS                        */
 /* -------------------------------------------------------------------------- */
-const CardSection = new Section({
-  items: initialCards,
-  renderer: (item) => {
-    const cardEl = new Card(item)
-    CardSection.addItems(cardEl.getView())
-  },
-  selector: selectors.cardSection,
-})
 
 /* -------------------------------------------------------------------------- */
 /*                         INTIALIZE ALL MY INSTANCES                         */
