b97a9cf566558397759b6916b01e990de9cc25d3
Ben Linn <benlinn26@gmail.com>
2022-12-29 12:53:19 -0800

    save changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,29 @@
 /* -------------------------------------------------------------------------- */
 /*                           IMPORT ALL THE CLASSES                           */
 /* -------------------------------------------------------------------------- */
+import './index.css'
+
+import { initialCards, selectors } from '../scripts/constants'
+import Card from '../scripts/Card'
+import FormValidator from '../scripts/FormValidator'
+import Section from '../scripts/Section'
 
 /* -------------------------------------------------------------------------- */
 /*                        CREATE INSTANCE OF THE CLASS                        */
 /* -------------------------------------------------------------------------- */
-
+const CardSection = new Section(
+  {
+    renderer: (item) => {
+      const cardEl = new Card(item, selectors.cardTemplate)
+      CardSection.addItem(cardEl.getView())
+    },
+  },
+  selectors.cardSection
+)
 /* -------------------------------------------------------------------------- */
 /*                         INTIALIZE ALL MY INSTANCES                         */
 /* -------------------------------------------------------------------------- */
-
+CardSection.renderItems(initialCards)
 /* -------------------------------------------------------------------------- */
 /*                                ALL THE REST                                */
 /* -------------------------------------------------------------------------- */
---
+++ src/scripts/section.js
@@ -0,0 +1,10 @@
+export default class Section {
+  constructor({ renderer, selector }) {
+    this._renderer = renderer
+    this._element = document.querySelector(selector)
+  }
+  renderItems(data) {
+    // use this._renderer to create the elements for rendering
+  }
+  addItems(item) {}
+}
