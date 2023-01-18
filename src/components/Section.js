7fec3db93ac0db7755ca863bf8e243685de5d929
createdbybenlinn <111088084+blinn26@users.noreply.github.com>
2023-01-18 11:49:50 -0800

    section

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,17 +1,17 @@
 class Section {
   constructor({ items, renderer }, containerSelector) {
-    this._renderer = renderer
-    this._items = items
-    this._container = document.querySelector(containerSelector)
+    this._renderer = renderer;
+    this._items = items;
+    this._container = document.querySelector(containerSelector);
   }
   renderItems() {
     this._items.forEach((item) => {
-      this._renderer(item)
-    })
+      this._renderer(item);
+    });
   }
   addItem(element) {
-    this._container.prepend(element)
+    this._container.prepend(element);
   }
 }
 
-export default Section
+export default Section;
